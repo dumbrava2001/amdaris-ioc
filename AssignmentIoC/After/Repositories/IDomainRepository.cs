c716c68532606e8c7479cafe9a355aa68d12d64b
DumbravaM <42375617+dumbrava2001@users.noreply.github.com>
2024-03-26 12:35:44 +0200

    Add domain repository

---
+++ AssignmentIoC/After/Repositories/IDomainRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentIoC.After.Repositories;
+
+public interface IDomainRepository
+{
+    void Add(string domain);
+    bool Contains(string domainToFind);
+    IEnumerable<string> GetAll();
+
+    void Delete(string domain);
+}
---
+++ AssignmentIoC/After/Repositories/Implementations/DomainListRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace AssignmentIoC.After.Repositories.Implementations;
+
+public class DomainListRepository : IDomainRepository
+{
+    private readonly List<string> _domainList = new() { "aol.com", "hotmail.com", "prodigy.com", "CompuServe.com" };
+
+    public void Add(string domain)
+    {
+        _domainList.Add(domain);
+    }
+
+    public bool Contains(string domainToFind)
+    {
+        return _domainList.Contains(domainToFind);
+    }
+
+    public IEnumerable<string> GetAll()
+    {
+        return new List<string>(_domainList);
+    }
+
+    public void Delete(string domain)
+    {
+        _domainList.Remove(domain);
+    }
+}
